5c2360df4ece596b40eb80105f873d68d748c0aa
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2020-11-04 11:25:08 +0530

    add camera and capture photo

--- app/screens/TabTwoScreen.tsx
+++ app/screens/TabTwoScreen.tsx
@@ -1,32 +1,103 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.js" />
-    </View>
-  );
+	const [hasPermission, setHasPermission] = React.useState<boolean | null>(
+		null
+	);
+	const ref = React.createRef<Camera>();
+	const [currentImage, setcurrentImage] = React.useState<string | null>(null);
+
+	React.useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestPermissionsAsync();
+			setHasPermission(status === 'granted');
+		})();
+	}, []);
+
+	if (hasPermission === null) {
+		return <View />;
+	}
+	if (hasPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
+
+	return (
+		<View style={{ flex: 1 }}>
+			{currentImage ? (
+				<View style={{ flex: 1 }}>
+					<Ionicons
+						name="md-arrow-back"
+						size={30}
+						style={{ padding: 8 }}
+						onPress={() => setcurrentImage(null)}
+					/>
+					<Image
+						source={{ uri: currentImage }}
+						style={{ flex: 1 }}
+						resizeMode="contain"
+					/>
+					<TouchableOpacity
+						style={{
+							alignSelf: 'center',
+							alignItems: 'center',
+							marginBottom: 16,
+							backgroundColor: 'white',
+							borderRadius: 4,
+							padding: 6,
+							paddingHorizontal: 24,
+							elevation: 2,
+						}}
+						onPress={() => {}}
+					>
+						<Text style={{ color: '#416ce1' }}>Send</Text>
+					</TouchableOpacity>
+				</View>
+			) : (
+				<Camera style={{ flex: 1 }} type={'back'} ref={ref} ratio="1:1">
+					<View
+						style={{
+							flex: 1,
+							backgroundColor: 'transparent',
+							flexDirection: 'row',
+							justifyContent: 'center',
+							marginBottom: 40,
+						}}
+					>
+						<TouchableOpacity
+							style={{
+								alignSelf: 'flex-end',
+								alignItems: 'center',
+								marginBottom: 16,
+								backgroundColor: 'white',
+								borderRadius: 4,
+								padding: 6,
+								paddingHorizontal: 24,
+							}}
+							onPress={() =>
+								ref.current?.takePictureAsync({
+									onPictureSaved: (pic) => {
+										setcurrentImage(pic.uri);
+									},
+								})
+							}
+						>
+							<Text style={{ color: '#416ce1' }}>Capture</Text>
+						</TouchableOpacity>
+					</View>
+				</Camera>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
--- app/types.tsx
+++ app/types.tsx
@@ -4,8 +4,8 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Home: undefined;
+  Camera: undefined;
 };
 
 export type TabOneParamList = {
